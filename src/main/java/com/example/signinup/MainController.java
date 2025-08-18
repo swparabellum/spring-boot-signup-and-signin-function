90d8774bb24bba7ef635310550e9f81c36829cf9
swparabellum <swparabellum@gmail.com>
2025-08-18 17:43:58 +0900

    기능추가: 계정삭제 기능추가 자기자신은 삭제 안되도록하며 다른 계정은 삭제 가능하게 구현함.

--- src/main/java/com/example/signinup/MainController.java
+++ src/main/java/com/example/signinup/MainController.java
@@ -1,14 +1,18 @@
 package com.example.signinup;
 
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+//import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
+import java.util.Objects;
 
 @Controller
 @AllArgsConstructor
@@ -24,6 +28,25 @@ public class MainController {
         return mainService.main(model,principal);
     }
 
+    @DeleteMapping("/api/users/{email}")
+    @ResponseBody
+    @Transactional
+    public ResponseEntity<String> deleteUser(@PathVariable String email, Principal principal) {
+        System.out.println("delete user: "+email);
+        if(Objects.equals(email, principal.getName())){
+            System.out.println("you can't delete yourself... failed to delete user: "+principal.getName());
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("you can't delete yourself");
+        }
 
-
+        try {
+            userRepository.deleteByEmail(email);
+            System.out.println("try delete user: "+email);
+            return ResponseEntity.ok("User deleted successfully");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("failed to delete user: "+email);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error deleting user");
+        }
+    }
 }
